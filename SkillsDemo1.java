c032a95e13dc3e03801c2504c3691af3429336ad
helloitsmurph <103203095+helloitsmurph@users.noreply.github.com>
2022-05-01 19:19:49 -0700

    Added in new tester file and one test

--- SkillsDemo1.java
+++ SkillsDemo1.java
@@ -1,3 +1,11 @@
 public class SkillsDemo1 {
 
+    public static int getAverage(int[] data){
+        //taking the average of a given set of data
+        int sum = 0;
+        for(int i = 0; i < data.length; i++) {
+            sum = sum + data[i];
+        }
+        return sum/data.length; 
+    }
 }
---
+++ SkillsDemo1Tester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillsDemo1Tester {
+    @Test
+    public void getAverageTest() {
+        int[] dataSet = {1,2,3,4,5};
+        assertEquals(4, SkillsDemo1.getAverage(dataSet));
+    }
+}
